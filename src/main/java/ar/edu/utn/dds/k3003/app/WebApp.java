6665e51f3bc7ac58dc149ecfc40921a72f47beae
AyluDubini <adubini@frba.utn.edu.ar>
2024-05-15 17:35:51 -0300

    WebApp iniciada

---
+++ src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
@@ -0,0 +1,48 @@
+package ar.edu.utn.dds.k3003.app;
+import ar.edu.utn.dds.k3003.clients.LogisticaProxy;
+import ar.edu.utn.dds.k3003.clients.ViandasProxy;
+import ar.edu.utn.dds.k3003.controller.ColaboradorController;
+import ar.edu.utn.dds.k3003.facades.dtos.Constants;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.javalin.Javalin;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class WebApp {
+    public static void main(String[] args) {
+
+        var env = System.getenv();
+        var objectMapper = createObjectMapper();
+        var fachada = new Fachada();
+        fachada.setViandasProxy(new ViandasProxy(objectMapper));
+        fachada.setLogisticaProxy(new LogisticaProxy(objectMapper));
+        var port = Integer.parseInt(env.getOrDefault("PORT", "8080"));
+
+        var app = Javalin.create().start(port);
+
+        var colaboradorController = new ColaboradorController(fachada);
+
+        app.post("/colaboradores", colaboradorController::agregar);
+        app.get("/colaboradores/{id}", colaboradorController::obtener);
+        app.patch("/colaboradores/{id}",colaboradorController::cambiarForma);//revisar
+        app.get("/colaboradores/{id}/puntos",colaboradorController::puntos);//revisar
+        app.put("/formula", colaboradorController::actualizarPuntos);//revisar
+    }
+
+    public static ObjectMapper createObjectMapper() {
+        var objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        var sdf = new SimpleDateFormat(Constants.DEFAULT_SERIALIZATION_FORMAT, Locale.getDefault());
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        objectMapper.setDateFormat(sdf);
+        return objectMapper;
+    }
+}
+
---
+++ src/main/java/ar/edu/utn/dds/k3003/controller/ColaboradorController.java
@@ -0,0 +1,77 @@
+package ar.edu.utn.dds.k3003.controller;
+
+import ar.edu.utn.dds.k3003.app.Fachada;
+import ar.edu.utn.dds.k3003.facades.dtos.ColaboradorDTO;
+import ar.edu.utn.dds.k3003.facades.dtos.FormaDeColaborarEnum;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import org.eclipse.jetty.http.HttpTester;
+
+import javax.lang.model.type.ArrayType;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
+public class ColaboradorController {
+    private final Fachada fachada;
+
+    public ColaboradorController(Fachada fachada) {
+        this.fachada = fachada;
+    }
+
+    public void agregar(Context context) {
+        var colaboradorDTO = context.bodyAsClass(ColaboradorDTO.class);
+        var colaboradorDTORta = this.fachada.agregar(colaboradorDTO);
+        context.json(colaboradorDTORta);
+        context.status(HttpStatus.CREATED);
+    }
+
+    public void obtener(Context context) {
+        var id = context.pathParamAsClass("id", Long.class).get();
+        try {
+            var colaboradorDTO = this.fachada.buscarXId(id);
+            context.json(colaboradorDTO);
+        } catch (NoSuchElementException ex) {
+            context.result(ex.getLocalizedMessage());
+            context.status(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public void cambiarForma(Context context) { //revisar
+        var id = context.pathParamAsClass("id", Long.class).get();
+        var forma = context.bodyAsClass(FormaDeColaborarEnum.class);
+        try{ var colaboradorDTO = this.fachada. modificar(id, List.of(forma));
+        context.json(colaboradorDTO);
+        } catch (NoSuchElementException ex) {
+            context.result(ex.getLocalizedMessage());
+            context.status(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public void puntos(Context context) {
+        var id = context.pathParamAsClass("id", Long.class).get();
+            try {var colaboradorDTO = this.fachada.puntos(id);
+            context.json(colaboradorDTO);
+            } catch (NoSuchElementException ex) {
+                context.result(ex.getLocalizedMessage());
+                context.status(HttpStatus.NOT_FOUND);
+            }
+    }
+
+    public void actualizarPuntos(Context context) {
+        ArrayList<Double> puntos = context.bodyAsClass(ArrayList.class);
+        Double pesosDonados = puntos.get(0);
+        Double viandasDistribuidas = puntos.get(1);
+        Double viandasDonadas= puntos.get(2);
+        Double tarjetasRepartidas= puntos.get(3);
+        Double heladerasActivas= puntos.get(4);
+        this.fachada.actualizarPesosPuntos(pesosDonados,
+                viandasDistribuidas,
+                viandasDonadas,
+                tarjetasRepartidas,
+                heladerasActivas);
+    }
+}
